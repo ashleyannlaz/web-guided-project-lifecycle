27678e5d4e2ad081ba37c4f53529601a4eea783e
ashleyannlaz <paperpastels@gmail.com>
2021-08-03 12:30:19 -0400

    Setup

--- review/src/AppFunc.js
+++ review/src/AppFunc.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 const AppFunc = () => {
+  console.log(hello);
   const [name, setName] = useState("Warren");
+
+  useEffect(() => {
+    console.log(`App has mounted`)
+  }, []);
   
   const handleClick = () => {
+    console.log('Change State');
     setName("Allison");
   };
-
+  console.log('Render DOM');
   return (
     <div>
       <h1>Hello {name}!</h1>
-  
+      <h2>Hello</h2>
       <button onClick={handleClick}>MAKE IT ALLISON!</button>
     </div>
   );
